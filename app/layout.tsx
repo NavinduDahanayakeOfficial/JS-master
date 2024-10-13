bfe47874e068ac62753e4d33a5fa6a76ece33110
Navindu <dahanayakenw.20@uom.lk>
2024-10-13 23:50:36 +0530

    improved metadata and changed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,27 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "JS Master",
-  description: "JS Master resources",
+   title: "JS Master",
+   description: "JS Master resources",
+   other: {
+      "theme-color": "#0d1117",
+      "color-scheme": "dark only",
+      "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+      "twitter:card": "summary_large_image",
+      "og:url": "https://jsm-pro-gray.vercel.app/",
+      "og:image":'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+      "og:type": "website"
+   },
 };
 
-export default function RootLayout({ children,}: Readonly<{children: React.ReactNode;}>) {
-  return (
-    <html lang="en">
-      <body className="min-h-screen bg-black-100 font-poppins">{children}</body>
-    </html>
-  );
+export default function RootLayout({
+   children,
+}: Readonly<{ children: React.ReactNode }>) {
+   return (
+      <html lang="en">
+         <body className="min-h-screen bg-black-100 font-poppins">
+            {children}
+         </body>
+      </html>
+   );
 }
